54db42fd6ab798be0a70e321c20dd9e79da10406
Arif <arif@Arifs-MBP.T-mobile.com>
2017-03-05 16:23:17 -0500

    Created LCVC and wired up page elements

--- AlphabetsBangla/LessonCompleteVC.swift
+++ AlphabetsBangla/LessonCompleteVC.swift
@@ -2,7 +2,7 @@
 //  LessonCompleteVC.swift
 //  AlphabetsBangla
 //
-//  Created by Arif  on 2/19/17.
+//  Created by Arif  on 3/4/17.
 //  Copyright © 2017 Arif . All rights reserved.
 //
 
@@ -10,26 +10,53 @@ import UIKit
 
 class LessonCompleteVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var lessonTitle: UILabel!
+    @IBOutlet weak var completeLabel: UILabel!
+    @IBOutlet weak var retryLabel: UIButton!
+    @IBOutlet weak var testLabel: UIButton!
+    
+    private var _lesson: Lesson!
+    
+    var lesson: Lesson {
+        get {
+            return _lesson
+        } set {
+            _lesson = newValue
+        }
     }
     
+    let attributes: [String: Any] = [NSStrokeWidthAttributeName: 2.0,
+                                     NSStrokeColorAttributeName: UIColor.white,
+                                     NSForegroundColorAttributeName: UIColor.red
+                                    ]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lessonTitle.text = lesson.lessonTitle
+        styleLabel(uilabel: completeLabel)
+        styleButton(uibutton: retryLabel)
+        styleButton(uibutton: testLabel)
+        
     }
-    */
+    
+    func styleLabel(uilabel: UILabel) {
+        uilabel.attributedText = NSAttributedString(string: completeLabel.text!, attributes: attributes)
+    }
+    
+    func styleButton(uibutton: UIButton) {
+        uibutton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        
+    }
 
+    @IBAction func retryBtn(_ sender: Any) {
+        
+    }
+
+    @IBAction func testBtn(_ sender: Any) {
+        
+    }
+    
 }
